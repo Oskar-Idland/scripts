ce78b07ecbe631ccbb34270915c9d8fff9f0f977
Oskar Idland <oskaridland@hotmail.com>
2024-01-08 12:38:38 +0100

    fish wallpaper & fish add functions

---
+++ add_fish_func.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/fish
+set name "add-wallpaper.fish"
+set path "$HOME/.config/fish/functions/"
+set func (string join \n '
+#!/usr/bin/fish
+####################################################################################
+# Functions which copies an image from the wallpaper folder into chosen theme folder
+####################################################################################
+
+function add-wallpaper
+    set img "$argv[1]"
+    set img_path "$HOME/Pictures/Wallpapers/"
+    
+    set options "Catppuccin-Mocha" \
+                "Graphite-Mono" \
+                "Tokyo-Night" \
+                "Rose-Pine" \
+                "Gruvbox-Retro" \
+                "Material-Sakura" \
+                "Decay-Green" \
+                "Cyberpunk-Edge" \
+                "Frosted-Glass" \
+                "Catppuccin-Latte"
+    set selected (printf "%s\n" $options | fzf --prompt="Select an option: ")
+    if test -n "$selected"
+        clear
+        cp $img_path$img $HOME/.config/swww/$selected/$img && 
+        echo "Added" $img "to" $selected"-theme"
+        
+    else
+        clear
+        echo "No option selected."
+    end
+end
+')
+printf "%s\n" $func > $path$name
