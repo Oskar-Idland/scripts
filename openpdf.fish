a76e79b27c0c7a314c36ffac9f7239447a8d75c1
Oskar Idland <oskaridland@hotmail.com>
2024-01-20 18:48:52 +0100

    fixed openpdf for zathura

--- openpdf.fish
+++ openpdf.fish
@@ -1,7 +1,8 @@
 function openpdf
     if test -f $argv[1]
-        open $argv[1] > /dev/null 2>&1
-        return 0
+        nohup zathura $argv[1] > /dev/null 2>&1 &
+        disown
+        exit
     else 
         echo "No such pdf as '$argv[1]'"
     end 
