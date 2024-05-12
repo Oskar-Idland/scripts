1a1a9065c85f2ffeb8cb96200ba6a11e93700280
Oskar Idland <oskaridland@hotmail.com>
2024-05-12 20:55:26 +0000

    fixed bug

--- code-version-swap.fish
+++ code-version-swap.fish
@@ -1,15 +1,20 @@
 #!/usr/bin/fish
 function code-version-swap
     set lnk (readlink /usr/bin/code)
+    set p     /usr/bin/code
+    set p_old /usr/bin/code_old
+    set p_new /usr/bin/code_new
 
-    if [ "$lnk" = "code_old" ]
-        sudo ln -sf code_new code
-    end
+    if [ "$lnk" = "$p_old" ]
+        sudo ln -sf $p_new $p
 
-    if [ "$lnk" = "code_new" ]
-        sudo ln -sf code_old code
-    end
+    else if [ "$lnk" = "code_new" ]
+        sudo ln -sf $p_old $p
+    
+    else
+        sudo ln -sf $p_old $p
+    end 
 
-    echo code  (readlink code)
+    echo "code  $(readlink $p)"
     echo (code -v)
 end
