59f6304ac27b8f419e6332fb138c1416d48d3c41
Oskar-Idland <oskaridland@hotmail.com>
2024-10-16 01:18:52 +0200

    search-folder also searches root

--- search-folders.fish
+++ search-folders.fish
@@ -1,8 +1,12 @@
 #!/usr/bin/fish
 
 function search-folders
+    # First apply on all files in the current folder
+    set_color -ou blue ; printf "\n ./\n" ; set_color normal
+    search-files $argv
     for folder in *
         if test -d $folder
+            # Recursively search for files in the folder. If found, executes the search-files function
             set_color -ou blue ; printf "\n $folder\n" ; set_color normal
             cd $folder
             search-files $argv
